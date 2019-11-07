b36b262974e31bd208e092db4ee2777aca9dcd4d
Martin Lee <martinlee1987@googlemail.com>
2019-11-07 12:01:31 +0000

    Test commit

--- lab-2-improve-code/src/index.js
+++ lab-2-improve-code/src/index.js
@@ -1,14 +1,14 @@
-
 module.exports = function greeting(i = 'my friend') {
-  if (typeof i == 'string') {
+  if (typeof i === 'string') {
     i = [i];
   }
+  
+  let nStr = '';
+  for (j in i) {
+    nStr = nStr + i[j] + ((parseInt(j) + 2 == i.length) ? ', and ' : ((parseInt(j) + 1 == i.length) ? '' : ', '));
+  }
 
-out = 'Hello, ';
+  if (nStr.toUpperCase() === nStr) return `HELLO ${nStr}!`;
 
-  nStr = '';
-  for (j in i ) { nStr = nStr + i[j] + ((parseInt(j)+2==i.length)?', and ': ((parseInt(j)+1==i.length)?'':', ')); }
-
-    if (nStr.toUpperCase() === nStr) return "HELLO " + nStr +  '!'
-
-  return "Hello, " + nStr +  '.'}
+  return `Hello, ${nStr}.`;
+};
